910f63f131ace60c8ffd375637560f6aef276e7b
leejaeyeob <ljy11124@naver.com>
2023-02-12 21:48:00 +0900

    ssdss

---
+++ Day 5 study/pstudy no 33 enumerate.py
@@ -0,0 +1,9 @@
+'''
+enumerate()
+    List, Tuple, String 등 자료형을 입력받으면
+    인덱스 값을 포함하는 enumerate 객체를 돌려준다.
+'''
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+print(len(months))
+for months, day in enumerate(months):
+    print('{}월 = {}일'.format(months+1, day))
