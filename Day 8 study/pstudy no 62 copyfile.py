68d2a89778c986ab63b0e40e7015dda646ea21ad
leejaeyeob <ljy11124@naver.com>
2023-02-28 19:05:23 +0900

    ssdss

---
+++ Day 8 study/pstudy no 62 copyfile.py
@@ -0,0 +1,16 @@
+'''
+파일 복사
+    원본 -> 버퍼 변수(Memory) -> 복사본
+
+b(binary mode): 해당 파일의 데이터를 바이너리 파일로 인식하고 입출력
+'''
+
+buffer_size = 1024  # 1024byte -> 1KB의미
+with open('../../Day07/Section13/hello.txt', 'rb') as source:
+    with open('hello2.txt', 'wb') as copy:
+        while True:
+            buffer = source.read(buffer_size)
+            if not buffer:
+                break
+            copy.write(buffer)
+print('hello2.txt 파일이 복사되었습니다.')
